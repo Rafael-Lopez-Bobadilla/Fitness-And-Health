2def53f9ef8e07fed80bf4060719c1b41ebe113e
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2023-12-04 22:08:14 +0100

    finished header v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
-    <h1>
-      Hola!
-    </h1>
+    <>
+      <Header />
+    </>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import s from './Header.module.scss'
+import ham from '../../images/svgs/ham.svg'
+import close from '../../images/svgs/close.svg'
+import { useState } from 'react'
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <header className={s.header}>
+      <nav className={s.nav}>
+        <a tabIndex={0} className={s.name}>Fitness & Health</a>
+        <img src={ham} className={s.ham} onClick={() => setIsOpen(!isOpen)} />
+        <div className={isOpen ? s.menuOpen : s.menu}>
+          <img src={close} className={s.close} onClick={() => setIsOpen(!isOpen)} />
+          <ul className={s.list}>
+            <li className={s.item}><a tabIndex={0}>About</a></li>
+            <li className={s.item}><a tabIndex={0}>Coaches</a></li>
+            <li className={s.item}><a tabIndex={0}>Photos</a></li>
+            <li className={s.item}><a tabIndex={0}>Pricing</a></li>
+            <li className={s.item}><a tabIndex={0}>FAQ</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  )
+}
+export default Header 
