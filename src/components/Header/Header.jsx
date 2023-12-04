38380892fa89e273ba7046b37479928117c40977
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2023-12-04 22:51:22 +0100

    changed styling in header menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <nav className={s.nav}>
         <a tabIndex={0} className={s.name}>Fitness & Health</a>
         <img src={ham} className={s.ham} onClick={() => setIsOpen(!isOpen)} />
-        <div className={isOpen ? s.menuOpen : s.menu}>
+        <div className={isOpen ? `${s.menu} ${s.open}` : s.menu}>
           <img src={close} className={s.close} onClick={() => setIsOpen(!isOpen)} />
           <ul className={s.list}>
             <li className={s.item}><a tabIndex={0}>About</a></li>
