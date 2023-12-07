95a7b453d26d3044bad25e6d5a94260f61690ac4
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2023-12-07 18:48:51 +0100

    added some more stuff

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,9 @@ import s from './About.module.scss'
 const About = () => {
   return (
     <section className={s.about}>
-      About
+      <div className={s.container}>
+        <h2 className={s.name}>About</h2>
+      </div>
     </section>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,21 @@
 import s from './Hero.module.scss'
-import hero1_large from '../../images/hero/hero1_large.jpg'
+import hero1_prev from '../../images/hero/hero1_prev.jpg'
+import arrowDown from '../../images/svgs/arrowDown.svg'
 const Hero = () => {
   return (
     <section className={s.hero}>
       <div className={s.container}>
-        <img src={hero1_large} className={s.img} />
+        <div className={s.leftOverlay}></div>
+        <img src={hero1_prev} className={s.img} />
         <div className={s.overlay}></div>
+        <div className={s.text}>
+          <h1>IT'S ALL ABOUT WHAT YOU CAN ACHIVE</h1>
+          <p>At Fitness & Health you train with more experience, more fun and more attention.</p>
+        </div>
+        <div className={s.iconContainer}>
+          <img src={arrowDown} className={s.icon} />
+        </div>
+        <div className={s.rightOverlay}></div>
       </div>
     </section>
   )
