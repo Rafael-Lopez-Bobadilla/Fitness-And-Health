10e2ef01aa2a9dad81dba315bcd8f5139c397233
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2023-12-10 14:59:43 +0100

    finished hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className={s.overlay}></div>
         <div className={s.text}>
           <h1>IT'S ALL ABOUT WHAT YOU CAN ACHIVE</h1>
-          <p>At Fitness & Health you train with more experience, more fun and more attention.</p>
+          <p>At Fitness & Health you train with more experience, more fun and more attention</p>
         </div>
         <div className={s.iconContainer}>
           <img src={arrowDown} className={s.icon} />
--- src/components/Hero/HeroPicture.jsx
+++ src/components/Hero/HeroPicture.jsx
@@ -1,26 +1,28 @@
-import hero1_large from '../../images/hero/hero1_large.jpg'
-import hero1_medium from '../../images/hero/hero1_medium.jpg'
-import hero1_small from '../../images/hero/hero1_small.jpg'
-import hero1_slarge from '../../images/hero/hero1_slarge.jpg'
-import hero1_smedium from '../../images/hero/hero1_smedium.jpg'
-import hero1_ssmall from '../../images/hero/hero1_ssmall.jpg'
+import hero_3000 from '../../images/hero/hero_3000.jpg'
+import hero_2000 from '../../images/hero/hero_2000.jpg'
+import hero_1500 from '../../images/hero/hero_1500.jpg'
+import hero_1000 from '../../images/hero/hero_1000.jpg'
+import hero_s2250 from '../../images/hero/hero_s2250.jpg'
+import hero_s1500 from '../../images/hero/hero_s1500.jpg'
+import hero_s1000 from '../../images/hero/hero_s1000.jpg'
+
 import s from './HeroPicture.module.scss'
 const HeroPicture = () => {
   return (
     <picture>
-      <source media='(max-width: 900px)'
-        srcSet={`
-          ${hero1_slarge} 3392w,
-          ${hero1_smedium} 1500w,
-          ${hero1_ssmall} 700w
-          `} />
-      <img src={hero1_large} className={s.img}
+      <source media='(max-width: 750px)' srcSet={`
+        ${hero_s2250} 2250w,
+        ${hero_s1500} 1500w,
+        ${hero_s1000} 1000w
+      `} />
+      <img src={hero_3000} className={s.img}
         srcSet={
-          `${hero1_large} 4728w,
-              ${hero1_medium} 3000w,
-              ${hero1_small} 1500w`
+          `${hero_3000} 3000w,
+          ${hero_2000} 2000w,
+          ${hero_1500} 1500w,
+          ${hero_1000} 1000w`
         }
-        sizes='(min-width: 1240px) 1240px' />
+        sizes='(min-width: 1240px) 992px' />
     </picture>
   )
 }
