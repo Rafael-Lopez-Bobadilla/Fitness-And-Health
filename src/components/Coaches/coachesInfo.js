38bfaed9d1b4218af43e70d7b3490f3b655e88fc
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2023-12-12 15:09:02 +0100

    starting to build coaches section

---
+++ src/components/Coaches/coachesInfo.js
@@ -0,0 +1,109 @@
+import coach5_500 from '../../images/coaches/coach5_500.jpg'
+import coach5_300 from '../../images/coaches/coach5_300.jpg'
+import coach5_150 from '../../images/coaches/coach5_150.jpg'
+export const coaches = [
+  {
+    name: 'Charles',
+    text: 'Charles is purpose driven with a strong passion for creating personalized fitness experiences. With over a decade of combined experience in physical therapy, fitness, health & wellness, he has empowered patients and clients of all levels of fitness. He understands the importance of creating an engaging and personalized fitness experience that keeps you motivated and excited to achieve your goals.',
+    specialties: [
+      'Strength Trainin',
+      'Mobility Training',
+      'HIIT & Circuit Training',
+      'Body Fat Loss'
+    ],
+    photo_500: coach5_500,
+    photo_300: coach5_300,
+    photo_150: coach5_150
+  },
+  {
+    name: 'Patrice',
+    text: 'Charles is purpose driven with a strong passion for creating personalized fitness experiences. With over a decade of combined experience in physical therapy, fitness, health & wellness, he has empowered patients and clients of all levels of fitness. He understands the importance of creating an engaging and personalized fitness experience that keeps you motivated and excited to achieve your goals.',
+    specialties: [
+      'Strength Trainin',
+      'Mobility Training',
+      'HIIT & Circuit Training',
+      'Body Fat Loss'
+    ],
+    photo_500: coach5_500,
+    photo_300: coach5_300,
+    photo_150: coach5_150
+  },
+  {
+    name: 'Drake',
+    text: 'Charles is purpose driven with a strong passion for creating personalized fitness experiences. With over a decade of combined experience in physical therapy, fitness, health & wellness, he has empowered patients and clients of all levels of fitness. He understands the importance of creating an engaging and personalized fitness experience that keeps you motivated and excited to achieve your goals.',
+    specialties: [
+      'Strength Trainin',
+      'Mobility Training',
+      'HIIT & Circuit Training',
+      'Body Fat Loss'
+    ],
+    photo_500: coach5_500,
+    photo_300: coach5_300,
+    photo_150: coach5_150
+  },
+  {
+    name: 'Ted',
+    text: 'Charles is purpose driven with a strong passion for creating personalized fitness experiences. With over a decade of combined experience in physical therapy, fitness, health & wellness, he has empowered patients and clients of all levels of fitness. He understands the importance of creating an engaging and personalized fitness experience that keeps you motivated and excited to achieve your goals.',
+    specialties: [
+      'Strength Trainin',
+      'Mobility Training',
+      'HIIT & Circuit Training',
+      'Body Fat Loss'
+    ],
+    photo_500: coach5_500,
+    photo_300: coach5_300,
+    photo_150: coach5_150
+  },
+  {
+    name: 'Oscar',
+    text: 'Charles is purpose driven with a strong passion for creating personalized fitness experiences. With over a decade of combined experience in physical therapy, fitness, health & wellness, he has empowered patients and clients of all levels of fitness. He understands the importance of creating an engaging and personalized fitness experience that keeps you motivated and excited to achieve your goals.',
+    specialties: [
+      'Strength Trainin',
+      'Mobility Training',
+      'HIIT & Circuit Training',
+      'Body Fat Loss'
+    ],
+    photo_500: coach5_500,
+    photo_300: coach5_300,
+    photo_150: coach5_150
+  },
+  {
+    name: 'Ramses',
+    text: 'Charles is purpose driven with a strong passion for creating personalized fitness experiences. With over a decade of combined experience in physical therapy, fitness, health & wellness, he has empowered patients and clients of all levels of fitness. He understands the importance of creating an engaging and personalized fitness experience that keeps you motivated and excited to achieve your goals.',
+    specialties: [
+      'Strength Trainin',
+      'Mobility Training',
+      'HIIT & Circuit Training',
+      'Body Fat Loss'
+    ],
+    photo_500: coach5_500,
+    photo_300: coach5_300,
+    photo_150: coach5_150
+  },
+  {
+    name: 'Roberto',
+    text: 'Charles is purpose driven with a strong passion for creating personalized fitness experiences. With over a decade of combined experience in physical therapy, fitness, health & wellness, he has empowered patients and clients of all levels of fitness. He understands the importance of creating an engaging and personalized fitness experience that keeps you motivated and excited to achieve your goals.',
+    specialties: [
+      'Strength Trainin',
+      'Mobility Training',
+      'HIIT & Circuit Training',
+      'Body Fat Loss'
+    ],
+    photo_500: coach5_500,
+    photo_300: coach5_300,
+    photo_150: coach5_150
+  },
+  {
+    name: 'Raul',
+    text: 'Charles is purpose driven with a strong passion for creating personalized fitness experiences. With over a decade of combined experience in physical therapy, fitness, health & wellness, he has empowered patients and clients of all levels of fitness. He understands the importance of creating an engaging and personalized fitness experience that keeps you motivated and excited to achieve your goals.',
+    specialties: [
+      'Strength Trainin',
+      'Mobility Training',
+      'HIIT & Circuit Training',
+      'Body Fat Loss'
+    ],
+    photo_500: coach5_500,
+    photo_300: coach5_300,
+    photo_150: coach5_150
+  }
+]
