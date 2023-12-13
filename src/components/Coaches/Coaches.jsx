ed88153e3518c6b49fe6004252c830aefc7601b4
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2023-12-13 16:07:02 +0100

    slider functionality almost finished

--- src/components/Coaches/Coaches.jsx
+++ src/components/Coaches/Coaches.jsx
@@ -1,29 +1,64 @@
 import SectionWrapper from '../SectionWrapper/SectionWrapper'
-import { useContext } from 'react'
+import { useContext, useRef, useState } from 'react'
 import { sectionsContext } from '../../SectionsProvider'
 import s from './Coaches.module.scss'
 import { coaches } from './coachesInfo'
 
 const Coaches = () => {
   const sectionsRefs = useContext(sectionsContext)
+  const isDragging = useRef(false)
+  const startPosition = useRef(0)
+  const animationRef = useRef()
+  const translate = useRef(0)
+  const position = useRef(0)
+  const index = useRef(0)
+  const slider = useRef()
+  const [currentCoach, setCurrentCoach] = useState(coaches[0].name)
+  const animation = () => {
+    slider.current.style.transform = `translateX(${translate.current}px)`
+    if (isDragging.current) requestAnimationFrame(animation)
+  }
   const handleStart = (e) => {
-    console.log('start')
+    slider.current.style.transition = 'none'
+    isDragging.current = true
+    startPosition.current = e.clientX
+    animationRef.current = requestAnimationFrame(animation)
   }
   const handleEnd = () => {
-    console.log('end')
+    if (isDragging.current) {
+      slider.current.style.transition = 'transform 0.3s linear'
+      position.current = (-slider.current.scrollWidth / coaches.length)
+      translate.current = position.current * index.current
+      cancelAnimationFrame(animationRef.current)
+      isDragging.current = false
+      position.current = translate.current
+    }
   }
   const handleMove = (e) => {
-    e.preventDefault()
-    console.log('move')
+    const translateTo = position.current + e.clientX - startPosition.current
+    if (translateTo < 0 && translateTo > -slider.current.scrollWidth) {
+      if (isDragging.current) {
+        if ((translate.current - position.current) < -30 && index.current < coaches.length - 1) {
+          index.current = index.current + 1
+          handleEnd()
+          return
+        }
+        if ((translate.current - position.current) > 30 && index.current > 0) {
+          index.current = index.current - 1
+          handleEnd()
+          return
+        }
+        translate.current = translateTo
+      }
+    }
   }
   return (
     <SectionWrapper heading='Coaches' ref={sectionsRefs[1]}>
       <p className={s.mainText}>Each of our carefully selected Personal Trainers embody the value & essence of Live Fit Gym. They come from a background of diverse experiences–professional & personal. Each Personal Trainer goes through rigorous training of Live Fit Gym’s values and offerings. Please refer to their areas of speciality if you have specific core goals & needs. We aren’t just excited to Live Fit & Live Well with you: we’re determined to build your inner & outer resiliency!</p>
-      <div className={s.carouselContainer}>
-        <div className={s.carousel}
+      <div className={s.sliderContainer}>
+        <div className={s.slider} ref={slider}
           onPointerDown={handleStart}
           onPointerUp={handleEnd}
-          onPointerLeave={handleEnd}
           onPointerMove={handleMove}
           onDragStart={e => e.preventDefault()}
           onContextMenu={e => e.preventDefault()}
@@ -38,6 +73,7 @@ const Coaches = () => {
             `}
                 sizes='150px'
                 onDragStart={e => e.preventDefault()}
+                className={currentCoach === coach.name ? `${s.img} ${s.current}` : s.img}
               />
             )
           })}
