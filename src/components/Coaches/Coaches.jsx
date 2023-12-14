16706e685ac12d52261e589bfe03793cec2a9693
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2023-12-14 13:48:14 +0100

    functional slider

--- src/components/Coaches/Coaches.jsx
+++ src/components/Coaches/Coaches.jsx
@@ -54,7 +54,7 @@ const Coaches = () => {
   }
   return (
     <SectionWrapper heading='Coaches' ref={sectionsRefs[1]}>
-      <p className={s.mainText}>Each of our carefully selected Personal Trainers embody the value & essence of Live Fit Gym. They come from a background of diverse experiences–professional & personal. Each Personal Trainer goes through rigorous training of Live Fit Gym’s values and offerings. Please refer to their areas of speciality if you have specific core goals & needs. We aren’t just excited to Live Fit & Live Well with you: we’re determined to build your inner & outer resiliency!</p>
+      <p className={s.mainText}>Each of our carefully selected Personal Trainers embody the value & essence of Fitness & Health. They come from a background of diverse experiences–professional & personal. Each Personal Trainer goes through rigorous training of Live Fit Gym’s values and offerings. Please refer to their areas of speciality if you have specific core goals & needs. We aren’t just excited to Live Fit & Live Well with you: we’re determined to build your inner & outer resiliency!</p>
       <div className={s.sliderContainer}>
         <div className={s.slider} ref={slider}
           onPointerDown={handleStart}
