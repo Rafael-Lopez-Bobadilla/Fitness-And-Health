e959dc430e3b8c03733a28c4b768033012792116
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2023-12-14 17:07:14 +0100

    started photos section

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -5,8 +5,10 @@ import s from './Photos.module.scss'
 const Photos = () => {
   const sectionsRefs = useContext(sectionsContext)
   return (
-    <SectionWrapper ref={sectionsRefs.photosRef} heading='Photos'>
-      <div className={s.placeHolder}></div>
+    <SectionWrapper ref={sectionsRefs[2]} heading='Photos'>
+      <div className={s.photos}>
+
+      </div>
     </SectionWrapper>
   )
 }
