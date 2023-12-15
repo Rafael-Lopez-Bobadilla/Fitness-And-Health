fb49310d9c217dadcda25ad4626198f5a2805367
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2023-12-15 15:24:20 +0100

    finished pricing section

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,11 +1,42 @@
 import SectionWrapper from "../SectionWrapper/SectionWrapper"
 import { useContext } from "react"
 import { sectionsContext } from "../../SectionsProvider"
+import s from './Pricing.module.scss'
+import { useState } from "react"
+import { cards, features } from "./pricingInfo"
+import tick from '../../images/svgs/tick.svg'
+import closeGray from '../../images/svgs/closeGray.svg'
 const Pricing = () => {
   const sectionsRefs = useContext(sectionsContext)
+  const [selected, setSelected] = useState('Platinum')
   return (
     <SectionWrapper ref={sectionsRefs[3]} heading={'Pricing'}>
-
+      <div className={s.selector}>
+        {cards.map(card => {
+          return <div className={card.name === selected ? `${s.option} ${s.selected}` : s.option}
+            key={card.name}
+            onClick={() => setSelected(card.name)}>
+            {card.name}</div>
+        })}
+      </div>
+      <div className={s.cards}>
+        {cards.map(card => {
+          return (
+            <div className={card.name === selected ? `${s.card} ${s.selected}` : s.card}>
+              <h3>{card.name}</h3>
+              <h4>{card.price}</h4>
+              {features.map((item, index) => {
+                return (
+                  <div className={index < card.features ? s.item : `${s.item} ${s.blur}`}>
+                    <img src={index < card.features ? tick : closeGray} />
+                    <span>{item}</span>
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
     </SectionWrapper>
   )
 }
---
+++ src/components/Pricing/pricingInfo.js
@@ -0,0 +1,28 @@
+
+export const cards = [
+  {
+    name: 'Platinum',
+    price: '$69/month',
+    features: 7
+  },
+  {
+    name: 'Advanced',
+    price: '$49/month',
+    features: 4
+  },
+  {
+    name: 'Basic',
+    price: '$29/month',
+    features: 3
+  }
+]
+
+export const features = [
+  'Personalized Fitness Program Design',
+  'Unlimited Team Training',
+  '4 Semi-Private Training Sessions',
+  'Monthly Nutrition Session',
+  'Quarterly Assessment & Goal Setting',
+  'Monthly 1-on-1 Private Training',
+  'Annual Corrective Exercise Session'
+]
