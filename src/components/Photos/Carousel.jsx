5ee7d5e2fb53ac608733aaa60ddfa740e7394163
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-01-02 18:23:45 +0100

    added drag functionality. Still need to fix some bugs

--- src/components/Photos/Carousel.jsx
+++ src/components/Photos/Carousel.jsx
@@ -10,15 +10,17 @@ const Carousel = ({ setIsOpen }) => {
       <Slides>
         {photosArray.map((photo, index) => {
           return (
-            <img className={s.img} key={index}
-              src={photo.photo_2000 ? photo.photo_2000 : photo.photo_1500}
-              srcSet={`
+            <div className={s.slide} key={index}>
+              <img className={s.img}
+                src={photo.photo_2000 ? photo.photo_2000 : photo.photo_1500}
+                srcSet={`
                   ${photo.photo_2000 ? `${photo.photo_2000} 2000w,` : ''}
                   ${photo.photo_1500 ? `${photo.photo_1500} 1500w,` : ''}
                   ${photo.photo_1000} 1000w
                 `}
-              sizes='(max-width: 700px) 100vw, 700px'
-            />
+                sizes='(max-width: 700px) 100vw, 700px'
+              />
+            </div>
           )
         })}
       </Slides>
--- src/components/Photos/Slides.jsx
+++ src/components/Photos/Slides.jsx
@@ -1,7 +1,67 @@
 import s from './Slides.module.scss'
+import { useRef, useEffect } from 'react'
 const Slides = ({ children }) => {
+  const slider = useRef()
+  const isDragging = useRef(false)
+  const dragStart = useRef(0)
+  const animationRef = useRef()
+  const index = useRef(0)
+  const position = useRef(0)
+  const translation = useRef(0)
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+  const handleResize = () => {
+    slider.current.style.transition = 'none'
+    setPositionByIndex()
+  }
+  const animation = () => {
+    slider.current.style.transform = `translateX(${translation.current}px)`
+    if (isDragging.current) requestAnimationFrame(animation)
+  }
+  const getPoition = (e) => {
+    const position = e.type.includes('mouse') ?
+      e.pageX :
+      e.touches[0].clientX
+    return position
+  }
+  const handleStart = (e) => {
+    slider.current.style.transition = 'none'
+    dragStart.current = getPoition(e)
+    isDragging.current = true
+    animationRef.current = requestAnimationFrame(animation)
+  }
+  const handleEnd = () => {
+    cancelAnimationFrame(animationRef.current)
+    isDragging.current = false
+    const movedBy = translation.current - position.current
+    if (movedBy < -100 && index.current < children.length - 1) index.current += 1
+    if (movedBy > 100 && index.current > 0) index.current -= 1
+    slider.current.style.transition = 'transform 0.3s'
+    setPositionByIndex()
+  }
+  const setPositionByIndex = () => {
+    translation.current = (-slider.current.scrollWidth / children.length) * index.current
+    position.current = translation.current
+    slider.current.style.transform = `translateX(${translation.current}px)`
+  }
+  const handleMove = (e) => {
+    if (isDragging.current) {
+      translation.current = getPoition(e) + position.current - dragStart.current
+    }
+  }
   return (
-    <div className={s.slides}>
+    <div className={s.slides} ref={slider}
+      onContextMenu={e => e.preventDefault()}
+      onTouchStart={handleStart}
+      onMouseDown={handleStart}
+      onMouseUp={handleEnd}
+      onTouchEnd={handleEnd}
+      onTouchMove={handleMove}
+      onMouseMove={handleMove}>
       {children}
     </div>
   )
